e93fa4fc684e6109ab92e894c28f54e13c73912a
Lancelot Gordon <lance032017@gmail.com>
2023-02-23 10:57:33 -0500

    add dummy plugin structure

---
+++ includes/class-sixaxis-wp-chatbot-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/techjackpot
+ * @since      1.0.0
+ *
+ * @package    Sixaxis_Wp_Chatbot
+ * @subpackage Sixaxis_Wp_Chatbot/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Sixaxis_Wp_Chatbot
+ * @subpackage Sixaxis_Wp_Chatbot/includes
+ * @author     Lancelot Gordon <lance032017@gmail.com>
+ */
+class Sixaxis_Wp_Chatbot_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ includes/class-sixaxis-wp-chatbot-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://github.com/techjackpot
+ * @since      1.0.0
+ *
+ * @package    Sixaxis_Wp_Chatbot
+ * @subpackage Sixaxis_Wp_Chatbot/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Sixaxis_Wp_Chatbot
+ * @subpackage Sixaxis_Wp_Chatbot/includes
+ * @author     Lancelot Gordon <lance032017@gmail.com>
+ */
+class Sixaxis_Wp_Chatbot_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'sixaxis-wp-chatbot',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
