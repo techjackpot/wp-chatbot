2e9b8acd50b04acaf0694fe72f0f139958c84e12
Lancelot Gordon <lance032017@gmail.com>
2023-02-23 17:28:47 -0500

    add chatbot

--- vue-ui/src/main.js
+++ vue-ui/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
-import ChatBot from './components/ChatBot';
+
+import Chat from 'vue-beautiful-chat'
+Vue.use(Chat)
 
 Vue.config.productionTip = false
+
+import ChatBot from './components/ChatBot';
 
 if (document.getElementById('chatbot-widget')) {
   new Vue({
